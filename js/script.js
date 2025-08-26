fcf898c476bde2114560d9ec8857c143f7328eca
Sasi <sasikiran.tt3011@gmail.com>
2025-08-26 10:07:55 +0530

    Added the sections

---
+++ js/script.js
@@ -0,0 +1,31 @@
+  const cards = document.querySelectorAll('.client-card');
+  let current = 0;
+
+  function showCard(index) {
+    cards.forEach((card, i) => {
+      card.classList.remove('active');
+      card.style.opacity = "0.5";
+      if (i === index) {
+        card.classList.add('active');
+        card.style.opacity = "1";
+      }
+    });
+  }
+
+  document.getElementById('next').addEventListener('click', () => {
+    current = (current + 1) % cards.length;
+    showCard(current);
+  });
+
+  document.getElementById('prev').addEventListener('click', () => {
+    current = (current - 1 + cards.length) % cards.length;
+    showCard(current);
+  });
+
+  showCard(current);
+
+  document.querySelectorAll('.faq-item').forEach(item => {
+      item.addEventListener('click', () => {
+        item.classList.toggle('active');
+      });
+    });
